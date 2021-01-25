6f72bda6406ce1cece92123cc455418123a10a02
Xin Ouyang <xin.ouyang@compass.com>
2021-01-25 15:23:52 +0800

    fix: run react native in workspace reports an error (#4)
    link: https://mixiaomi.pingcode.com/agile/items/600e6f5bc7f59c849fe25498

--- platform/mac/metro.config.js
+++ platform/mac/metro.config.js
@@ -4,8 +4,19 @@
  *
  * @format
  */
+const {
+  extraNodeModules,
+  watchFolders,
+} = require('react-native-yarn-workspaces-v2')(__dirname);
 
+console.log('watchFolders', watchFolders);
+console.log('extraNodeModules', extraNodeModules);
 module.exports = {
+  watchFolders,
+  resolver: {
+    extraNodeModules,
+  },
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
